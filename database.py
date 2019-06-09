64ebd195379559fb2a8732f27fc4af111dc66dd4
gal1260 <gantar.gal@gmail.com>
2019-06-09 21:32:08 +0200

    Added email confirmation, user info correction, minor additions

--- database.py
+++ database.py
@@ -1,24 +1,27 @@
-import os
-import psycopg2
+from mail import send_conformation
 
-DATABASE_URL = os.environ['DATABASE_URL']
+def establish_connection():
+    import os
+    import psycopg2
+
+    DATABASE_URL = os.environ['DATABASE_URL']
+
+    return psycopg2.connect(DATABASE_URL, sslmode='require')
 
 def hash_password(password):
     import bcrypt
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt(15))
 
-def establish_connection():
-    return psycopg2.connect(DATABASE_URL, sslmode='require')
-
-def confirm_data(name, surname, email, number):
+def confirm_data(name="a", surname="a", email="g@g.g", number="0"):
     import re
-    if not re.search(r"^[a-zA-Z\ ]+$", name):
+
+    if not re.search(r"^[a-zA-Z\ čšž]+$", name) and len(name) <= 100:
         return "Name must only contain characters"
-    elif not re.search(r"^[a-zA-Z\ ]+$", surname):
+    elif not re.search(r"^[a-zA-Z\ čšž]+$", surname) and len(surname) <= 100:
         return "Surname must only contain characters"
-    elif not re.search(r"[^@]+@[^@]+\.[^@]+", email):
+    elif not re.search(r"[^@]+@[^@]+\.[^@]+", email) and len(email) <= 30:
         return "Invalid email address"
-    elif not re.search(r"^[0-9\ \+]+$", number):
+    elif not re.search(r"^[0-9\ \+]+$", number) and len(number) <= 20:
         return "Invalid phone number"
 
     return False #No mistakes found
@@ -39,10 +42,10 @@ def insert_new_user(name, surname, password, email, number):
     connection = establish_connection()
     cursor = connection.cursor()
 
-    cursor.execute("""
+    cursor.execute("""\
                     INSERT INTO Users (Name, Surname, Password, Email, Tel, Role)
                     VALUES(%s, %s, %s, %s, %s, %s);
-                    """, (name, surname, hash_password(password).decode(), email, number, "admin"))
+                    """, (name, surname, hash_password(password).decode(), email, number, "user"))
 
     cursor.close()
     connection.commit()
@@ -53,7 +56,6 @@ def check_login(email, password):
     connection = establish_connection()
     cursor = connection.cursor()
     cursor.execute("""SELECT Password FROM Users WHERE Email = %s ;""", (email,)) #(x,) forces python to make a touple
-    connection.commit()
 
     response = cursor.fetchone()
     if not response:
@@ -71,7 +73,7 @@ def list_database():
     connection = establish_connection()
     cursor = connection.cursor()
 
-    cursor.execute("SELECT * FROM Users")
+    cursor.execute("SELECT * FROM Users ORDER BY Surname")
     response = cursor.fetchall()
 
     cursor.close()
@@ -80,6 +82,83 @@ def list_database():
 
     return response
 
+def get_user_data(email):
+    connection = establish_connection()
+    cursor = connection.cursor()
+
+    cursor.execute("SELECT * FROM Users WHERE Email = %s;", (email,))
+
+    data = cursor.fetchone()
+    cursor.close()
+    connection.close()
+    return data
+
+def change_data(datatype_index, user_email, data):
+    connection = establish_connection()
+    cursor = connection.cursor()
+
+    if datatype_index == 0:
+        cursor.execute("UPDATE Users SET password = %s WHERE Email = %s", (hash_password(data), user_email))
+    elif datatype_index == 1:
+        cursor.execute("UPDATE Users SET Name = %s WHERE Email = %s", (data, user_email))
+    elif datatype_index == 2:
+        cursor.execute("UPDATE Users SET Surname = %s WHERE Email = %s", (data, user_email))
+    elif datatype_index == 3:
+        cursor.execute("UPDATE Users SET Tel = %s WHERE Email = %s", (data, user_email))
+
+    cursor.close()
+    connection.commit()
+    connection.close()
+
+def email_conformation(email):
+    """Creates email validation code"""
+    from string import ascii_letters, digits
+    from random import choice
+    import datetime
+
+    code = "".join([choice(ascii_letters+digits) for i in range(50)])
+    confirmation_link = "http://galgantar.tk/confirmation/" + code
+
+    time = datetime.datetime.utcnow().strftime('%Y-%m-%d')
+
+    connection = establish_connection()
+    cursor = connection.cursor()
+    cursor.execute("""\
+                INSERT INTO Confirmations (Email, Code, Creation)
+                VALUES (%s, %s, %s)
+                """, (email, code, time))
+    cursor.close()
+
+    send_conformation(email, confirmation_link)
+
+    connection.commit()
+    connection.close()
+
+def confirm_email(code):
+    """Validates email confirmation code"""
+    connection = establish_connection()
+    cursor = connection.cursor()
+
+    cursor.execute("SELECT Email FROM Confirmations WHERE Code = %s", (code,))
+
+    try:
+        email = cursor.fetchone()[0]
+        cursor.execute("UPDATE Users SET Confirmed = TRUE WHERE Email = %s", (email,))
+        cursor.execute("DELETE FROM Confirmations WHERE Email = %s", (email,))
+        cursor.close()
+        connection.commit()
+        connection.close()
+        return True
+
+    except TypeError:
+        pass
+
+    cursor.close()
+    connection.commit()
+    connection.close()
+    return False
+
+
 def manual_execute():
     code = open("query.sql", "r").read()
     connection = establish_connection()
@@ -87,7 +166,11 @@ def manual_execute():
 
     cursor.execute(code)
 
-    print(cursor.fetchall())
+
+
+    if cursor.statusmessage.split()[0] == "SELECT":
+        response = cursor.fetchall()
+        print(response)
 
     cursor.close()
     connection.commit()
@@ -95,5 +178,5 @@ def manual_execute():
 
 
 if __name__ == "__main__":
-    #list_database()
+    #print(list_database())
     manual_execute()
--- main.py
+++ main.py
@@ -1,22 +1,18 @@
 from flask import Flask, render_template, session, request, redirect
-from database import insert_new_user, check_login, confirm_data, user_is_new, list_database
+from database import insert_new_user, check_login, confirm_data, user_is_new, list_database, get_user_data, change_data, email_conformation, confirm_email
 
 app = Flask(__name__)
 app.secret_key = "FHCqR4tvmOpgbYHWXtbe"
 
 @app.route("/")
-def index():
-    return render_template("pages/index.html")
-
-@app.route("/razred")
 def razred():
 
     if not session.get("user"):
-        return redirect("/razred/login")
+        return redirect("/login")
     else:
         return render_template("pages/class.html", email=session["user"])
 
-@app.route("/razred/register", methods=["GET", "POST"])
+@app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "GET":
         return render_template("pages/register.html")
@@ -43,9 +39,10 @@ def register():
             return render_template("pages/register.html", error=is_new)
         else:
             insert_new_user(name, surname, password, email, number)
-            return redirect("/razred/login")
+            email_conformation(email) #send confirmation link
+            return redirect("/login")
 
-@app.route("/razred/login", methods=["GET", "POST"])
+@app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "GET":
         return render_template("pages/login.html")
@@ -56,20 +53,62 @@ def login():
 
         if email and password and check_login(email, password):
             session["user"] = email
-            return redirect("/razred")
+            return redirect("/")
         else:
             return render_template("pages/login.html", error="Wrong username or password")
 
-@app.route("/razred/logout")
+@app.route("/logout")
 def logout():
     session["user"] = None
-    return redirect("/razred")
+    return redirect("/")
 
-@app.route("/razred/database")
+@app.route("/database")
 def database():
     seznam = list_database()
 
     return render_template("pages/database.html", seznam=seznam)
+
+@app.route("/profile", methods=["GET", "POST"])
+def profile():
+    if request.method == "GET":
+        data = get_user_data(session["user"])
+        return render_template("pages/profile.html", data=[data])
+
+    elif request.method == "POST":
+        passwords = (request.form.get("password1"), request.form.get("password2"))
+        name = request.form.get("name")
+        surname = request.form.get("surname")
+        number = request.form.get("number")
+
+        if passwords[0] and passwords[0] == passwords[1]:
+            change_data(0, session["user"], passwords[0])
+
+        elif name and not confirm_data(name=name):
+            change_data(1, session["user"], name)
+
+        elif surname and not confirm_data(surname=surname):
+            change_data(2, session["user"], surname)
+
+        elif number and not confirm_data(number=number):
+            change_data(3, session["user"], number)
+        else:
+            data = get_user_data(session["user"])
+            return render_template("pages/profile.html", data=[data], error="Wrong data")
+
+        return redirect("/profile")
+
+@app.route("/confirmation/<code>")
+def confirmation(code):
+    if confirm_email(code):
+        return redirect("/profile")
+    else:
+        return render_template("pages/invalid_confirmation.html")
+
+
+@app.route("/resend")
+def resend():
+    email_conformation(session["user"])
+    return render_template("pages/resend_confirmation.html", email=session["user"])
 
 if __name__ == "__main__":
     app.run(debug=True)
