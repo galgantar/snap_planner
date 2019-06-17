6fd6de2a9eeacc9547e99b0d9632b5e17031b0f9
gal1260 <gantar.gal@gmail.com>
2019-06-17 15:53:15 +0200

    Added unit tests

---
+++ tests.py
@@ -0,0 +1,67 @@
+import unittest
+import main
+
+class Test(unittest.TestCase):
+    def setUp(self):
+        main.app.testing = True
+        self.app = main.app.test_client()
+
+    def tearDown(self):
+        main.app.testing = False
+
+    def testLoading(self):
+        response = self.app.get("/")
+        assert response.status_code == 302
+
+    def testRegister(self):
+        get_response = self.app.get("/register")
+        assert get_response.status_code == 200
+
+    def testLogin(self):
+        get_response = self.app.get("/login")
+        assert get_response.status_code == 200
+
+        post_response = self.app.post("/login", data={"email" : "test.test@gmail.com", "password" : "a"})
+        assert b"Wrong username or password" in post_response.data
+
+    def testLogout(self):
+        response = self.app.get("/")
+        assert response.status_code == 302
+
+    def testDatabaseList(self):
+        response = self.app.get("/database")
+        assert response.status_code == 302
+
+    def testProfile(self):
+        response = self.app.get("/profile")
+        assert response.status_code == 302
+
+        post_response = self.app.post("/profile")
+        assert post_response.status_code == 302
+
+    def testConfirmation(self):
+        response = self.app.get("/confirmation")
+        assert response.status_code == 302
+
+        response = self.app.get("/confirmation/test")
+        assert  response.status_code == 200
+
+    def testResend(self):
+        response = self.app.get("/resend")
+        assert response.status_code == 302
+
+    def testPassword(self):
+        response = self.app.get("/password")
+        assert response.status_code == 200
+
+        post_response = self.app.post("/password", data={"email":"test.test@gmail.com"})
+        assert post_response.status_code == 302
+
+    def testPasswordCode(self):
+        response = self.app.get("/password/test?email=test.test<at>gmail.com")
+        assert response.status_code == 200
+
+        post_response = self.app.post("/password/test?email=test.test<at>gmail.com", data={"password1":"123", "password2":"123"})
+        assert response.status_code == 200
+
+unittest.main()
